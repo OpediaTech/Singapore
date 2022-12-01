159e928e14ba9fd43a43fc3221c33d8d614ca89e
nipaaa <akternipa941@gmail.com>
2022-12-01 12:15:45 -0800

    supporting organization array add

---
+++ singapore/pages/SupportingOrganizations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SupportingOrganizations = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SupportingOrganizations;
