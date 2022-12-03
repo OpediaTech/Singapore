38191c3d7b86fd1093c465550e781c8b86ae2b35
nipaaa <akternipa941@gmail.com>
2022-12-02 17:59:05 -0800

    speaker section icon change

--- singapore/components/Speakers/SpeakerCard.js
+++ singapore/components/Speakers/SpeakerCard.js
@@ -4,22 +4,34 @@ import instagram from "../../public/speakers/icon/instagram.png";
 import twitter from "../../public/speakers/icon/twitter.png";
 import Image from "next/image";
 import Card from "react-bootstrap/Card";
+import {CgFacebook} from "react-icons/cg";
+import {BsInstagram, BsTwitter} from "react-icons/bs";
 
 const SpeakerCard = ({ speaker }) => {
   return (
     <div className="col-12 col-md-6 col-lg-3">
       <Card className="speakers border-0">
-        <Image src={speaker.pic} alt="image" height="280" width="280" />
+        <div className="speakerImg">
+              <Image className="ppp" src={speaker.pic} alt="image" />
+              <div className="speaker__hov">
+                <p>
+                  {speaker.designation} <strong>{speaker.institude}</strong>
+                </p>
+                <p>
+                  Principle Scientest at <strong>IOM in Edinburgh</strong>{" "}
+                </p>
+              </div>
+        </div>
         <Card.Body className="text-center">
           <Card.Title className="name fw-bold">{speaker.name}</Card.Title>
           <Card.Text className="designation">
             {speaker.designation}{" "}
             <span className="fw-bold">{speaker.institude}</span>
           </Card.Text>
-          <div className="icon d-flex align-items-center justify-content-center text-white">
+          {/* <div className="icon d-flex align-items-center justify-content-center text-white">
             <a href="">
               {" "}
-              <Image src={facebook} />
+              <Image className="" src={facebook} />
             </a>
             <a href="">
               {" "}
@@ -29,13 +41,24 @@ const SpeakerCard = ({ speaker }) => {
               {" "}
               <Image src={twitter} />
             </a>
+          </div> */}
+          <div className="icon d-flex align-items-center justify-content-center text-white">
+            <a href="">
+              {" "}
+              <CgFacebook/>
+            </a>
+            <a href="">
+              {" "}
+              <BsInstagram/>
+            </a>
+            <a href="">
+              {" "}
+              <BsTwitter/>
+            </a>
           </div>
         </Card.Body>
 
-        <div className="speaker__hov">
-          <p>{speaker.designation} <strong>{speaker.institude}</strong></p>
-          <p>Principle Scientest at <strong>IOM in Edinburgh</strong> </p>
-        </div>
+        
       </Card>
     </div>
   );
