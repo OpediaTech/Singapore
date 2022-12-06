df2e57b1720b1764d3aa41d0cb5456654d536368
nipaaa <akternipa941@gmail.com>
2022-12-05 19:45:18 -0800

    update

--- singapore/components/SupportingOgzn/SupportingOgn.js
+++ singapore/components/SupportingOgzn/SupportingOgn.js
@@ -98,9 +98,10 @@ const SupportingOgn = ({ length }) => {
         {organizations.slice(0, length).map((organization, index) => (
           <div key={index} className="col-6 col-lg-6">
             <div className="card shadow-lgN p-3 p-xl-5 accomodation_content">
-              <Image src={organization.pic} alt="organization img" />
+              <Image className="img-fluid" src={organization.pic} alt="organization img" />
               <h4 className="fw-bold my-4 black_title inst__title text-center text-sm-start">{organization.title}</h4>
-              <p className="inst__p">{organization.description}</p>
+              <p className="inst__p d-md-none">{organization.description.slice(0,100)} <span className="fw-bold">See More ...</span></p>
+              <p className="inst__p d-none d-md-block">{organization.description}</p>
               <a href="">
                 <Button className="learn_more_btn border-0 mt-4 fw-bold ">
                   Visit Website
